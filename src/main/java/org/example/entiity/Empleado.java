352d2e076a3c215753160b2129fe89b28cc45c67
SUITE <mail@mail.com>
2025-07-19 18:52:44 -0500

    - Employee entity mapped.

--- src/main/java/org/example/entiity/Empleado.java
+++ src/main/java/org/example/entiity/Empleado.java
@@ -1,18 +1,63 @@
 package org.example.entiity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 @Entity
 @Table(name = "employee")
 public class Empleado {
-
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name= "nombre", length = 180, nullable = false)
     private String nombre;
+
+    @Column(name = "fecha_ingreso")
     private LocalDate fechaIngreso;
+
+    @Column(name = "salario")
     private Double salario;
 
+    public Empleado() {
+    }
 
+    public Empleado(LocalDate fechaIngreso, long id, String nombre, Double salario) {
+        this.fechaIngreso = fechaIngreso;
+        this.id = id;
+        this.nombre = nombre;
+        this.salario = salario;
+    }
 
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
 }
